dd12570f9c84272d55482d1fb24880cc8c00fc9c
Alois~ <moaugustin@proton.me>
2025-06-02 12:31:45 +0200

    Fixed Socket AGAIN

--- src/socket.js
+++ src/socket.js
@@ -1,3 +1,5 @@
 import { io } from 'socket.io-client';
 
-export const socket = io("https://sound.hackrland.dev");
+const URL = process.env.SERVER === 'true' ? 'https://sound.hackrland.dev' : 'http://localhost:3000';
+
+export const socket = io(URL);
