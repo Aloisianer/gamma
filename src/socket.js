9e8b5fa7c7fa6a1c29c605c324d665ff521b8dfc
Alois~ <moaugustin@proton.me>
2025-06-02 12:26:00 +0200

    Socket stuff

--- src/socket.js
+++ src/socket.js
@@ -1,5 +1,3 @@
 import { io } from 'socket.io-client';
 
-const URL = process.env.SERVER === 'true' ? 'https://sound.hackrland.dev' : 'http://localhost:3000';
-
-export const socket = io(URL);
+export const socket = io();
